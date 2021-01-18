c3343ed26b2b55f60780f2e10b3f7d688323c8a9
Sai Petluri <spetluri.1@gmail.com>
2021-01-18 03:16:35 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,12 @@
 # Write your #display_rainbow method here
+def display_rainbow(rainbow)
+  
+  rainbow_abbrev = ["R","O","Y","G","B","I","V"]
+  
+  puts("#{rainbow_abbrev[0]}: #{rainbow[0]}, #{rainbow_abbrev[1]}: #{rainbow[1]}, #{rainbow_abbrev[2]}: #{rainbow[2]}, #{rainbow_abbrev[3]}: #{rainbow[3]}, #{rainbow_abbrev[4]}: #{rainbow[4]}, #{rainbow_abbrev[5]}: #{rainbow[5]}, #{rainbow_abbrev[6]}: #{rainbow[6]}")
+  
+  
+end
+
+
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
